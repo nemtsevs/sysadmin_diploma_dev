5a4613d052c4ed8b4149372f0e1d84c8042e2960
nemtsevs <nemstani@yandex.ru>
2025-06-26 01:12:18 +0300

    ans

--- sysadmin_diploma_dev.sh
+++ sysadmin_diploma_dev.sh
@@ -157,3 +157,19 @@ yamllint php_install/tasks/main.yml
 ansible-playbook --syntax-check -i inventory.yaml playbook.yaml
 
 ansible-playbook playbook.yaml -i inventory.yaml --tags="php_install"
+
+
+nano playbook.yaml
+ansible-galaxy init mediawiki_install
+
+mkdir mediawiki_install/templates
+nano mediawiki_install/templates/local_settings.j2
+```
+"{{ hostvars['vm2']['ansible_host'] }}"
+```
+
+yamllint playbook.yaml
+yamllint mediawiki_install/tasks/main.yml
+ansible-playbook --syntax-check -i inventory.yaml playbook.yaml
+
+ansible-playbook playbook.yaml -i inventory.yaml --tags="mediawiki_install"

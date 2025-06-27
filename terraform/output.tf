ac2434251493c19495169c88bae5710daae3801b
nemtsevs <nemstani@yandex.ru>
2025-06-28 01:26:32 +0300

    yappm2gp

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,3 +5,10 @@ output "vm_ip" {
 output "vm_nat_ip" {
   value = { for k, v in yandex_compute_instance.virtual_machine : k => v.network_interface.0.nat_ip_address }
 }
+
+output "s3_bucket_info" {
+  value = {
+    name     = yandex_storage_bucket.ice_bucket.bucket
+    domain   = yandex_storage_bucket.ice_bucket.bucket_domain_name
+  }
+}
